85876bea7821539e0636fa8c231f6c782fa5596a
Daniel Oliveira <danielolivep@gmail.com>
2021-06-28 20:29:27 -0300

    Add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -12,10 +12,38 @@ describe('<Logo />', () => {
     })
   })
 
+  it('should render a white label when color is passed', () => {
+    renderWithTheme(<Logo color="white" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a normal logo', () => {
+    renderWithTheme(<Logo size="normal" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
