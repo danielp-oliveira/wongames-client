17fbc69e8d92cc7d15c802c9e862ec402bc655a3
Daniel Oliveira <danielolivep@gmail.com>
2021-06-30 21:10:16 -0300

    Add Button size tests

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>By now</Button>)
+
+    expect(screen.getByRole('button', { name: /By now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">By now</Button>)
+
+    expect(screen.getByRole('button', { name: /By now/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">By now</Button>)
+
+    expect(screen.getByRole('button', { name: /By now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
+  })
+})
